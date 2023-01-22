04f58073e7696f3855f6699f8739a92aaf8baf96
정승우 <sunjw0531@gmail.com>
2023-01-22 22:14:28 +0900

    Styled Components animations and pseudo selectors
    Styled Components animations and pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
-      <Box as="a" href="/">
-        asdfasdf
+    <Wrapper>
+      <Box>
+        <span>%%%%</span>
       </Box>
-      <Input />
-    </Father>
+    </Wrapper>
   );
 }
 
 export default App;
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
+const rotateAni = keyframes`
+  0%{
+    transform:rotate(0deg);
+    border-radius : 0px;
+  }
+  50%{
+    border-radius : 100px;
+  }
+  100%{
+    transform : rotate(360deg);
+    border-radius : 0px;
+  }
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-  background-color: yellow;
+  display: flex;
+  background-color: tomato;
+  height: 200px;
+  width: 200px;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAni} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
