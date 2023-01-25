ad065faffc7c8b1110d6d558245189c5d5b2a6cb
정승우 <sunjw0531@gmail.com>
2023-01-25 22:28:20 +0900

    Styled Components Themes
    import ThemeProvider from styled-components to make darkmode

--- src/App.js
+++ src/App.js
@@ -3,59 +3,22 @@ import styled, { keyframes } from 'styled-components';
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <TextTag>%%%%</TextTag>
-      </Box>
-      <TextTag>!!!!</TextTag>
-      <AnotherBox>&&&&&&</AnotherBox>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
 
 export default App;
 
-const FlexDiv = styled.div`
+const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Wrapper = styled(FlexDiv)`
   height: 100vh;
   width: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotateAni = keyframes`
-  0%{
-    transform:rotate(0deg);
-    border-radius : 0px;
-  }
-  50%{
-    border-radius : 100px;
-  }
-  100%{
-    transform : rotate(360deg);
-    border-radius : 0px;
-  }
-`;
-
-const TextTag = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled(FlexDiv)`
-  display: flex;
-  background-color: tomato;
-  height: 200px;
-  width: 200px;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAni} 1s linear infinite;
-  ${TextTag}:hover {
-    font-size: 90px;
-  }
-`;
-
-const AnotherBox = styled(Box)`
-  background-color: blue;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
