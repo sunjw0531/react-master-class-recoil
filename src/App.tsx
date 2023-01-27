eacc4176ba4985e848ba5dd90f7637ef83195157
정승우 <sunjw0531@gmail.com>
2023-01-27 16:13:58 +0900

    TypeScript : optional props
    using '?' and '??'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import styled from 'styled-components';
 import Circle from './Circle';
 
 function App() {
   return (
     <div>
-      <Circle bgColor="red" />
-      <Circle bgColor="blue" />
+      <Circle bgColor="red" borderColor="blue" />
+      <Circle bgColor="blue" text="Hello" />
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,18 +1,34 @@
 import styled from 'styled-components';
 
-interface CircleProps {
+interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
-const Container = styled.div<CircleProps>`
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
-const Circle = ({ bgColor }: CircleProps) => {
-  return <Container bgColor={bgColor} />;
+const Circle = ({
+  bgColor,
+  borderColor,
+  text = 'default Text',
+}: CircleProps) => {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 };
 
 export default Circle;
