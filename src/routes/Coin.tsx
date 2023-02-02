71329a1c2f2b26f48638e8af03868960d9f33acc
정승우 <sunjw0531@gmail.com>
2023-02-02 15:41:27 +0900

    Crypto Tracker : Coin data
    Get coin data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -13,7 +13,22 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const location = useLocation();
   const state = location.state as RouteState;
-  console.log(state);
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPrice(priceData);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
