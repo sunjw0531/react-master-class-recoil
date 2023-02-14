ee90a25804b22e6990ac7eb932fd94632752691d
정승우 <sunjw0531@gmail.com>
2023-02-14 15:05:17 +0900

    State Management : Come Back to Todo list

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,42 +1,8 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-// function TodoList() {
-//   const [toDo, setToDo] = useState('');
-//   const [toDoError, setToDoError] = useState('');
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDoError('');
-//     setToDo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     if (toDo.length < 10) {
-//       return setToDoError('To do should be longer');
-//     }
-
-//     console.log('submit');
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-//         <button>Add</button>
-//         {toDoError !== '' ? toDoError : null}
-//       </form>
-//     </div>
-//   );
-// }
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
 function TodoList() {
@@ -44,83 +10,23 @@ function TodoList() {
     register,
     handleSubmit,
     formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: '@naver.com',
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        'password1',
-        { message: 'password are not the same' },
-        { shouldFocus: true }
-      );
-    }
-    // setError('extraError', { message: 'Server offline' });
+    setValue,
+  } = useForm<IForm>();
+
+  const handleValid = (data: IForm) => {
+    console.log('add to do', data.toDo);
+    setValue('toDo', '');
   };
-  console.log(errors);
   return (
     <div>
-      <form
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={handleSubmit(onValid)}
-      >
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register('email', {
-            required: 'Email is required',
-            // pattern: /^[A-Za-z0-9._%+-]+@naver.com$/,
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: 'Only naver.com emails',
-            },
+          {...register('toDo', {
+            required: 'Please write a to do',
           })}
-          placeholder="Email"
+          placeholder="Write a to do"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register('firstName', {
-            required: 'write here',
-
-            validate: {
-              noJohn: (value) =>
-                value.includes('john') ? 'No john name' : true,
-              noPaul: (value) =>
-                value?.includes('paul') ? 'no Paul name' : true,
-            },
-          })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register('lastName', { required: 'write here' })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register('username', { required: 'write here', minLength: 10 })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register('password', { required: 'write here', minLength: 5 })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register('password1', {
-            required: 'password is required',
-            minLength: {
-              value: 5,
-              message: 'Your password is too short.',
-            },
-          })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
