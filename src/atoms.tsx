f3be917a52551524008d5df0bdb61221ebaea843
정승우 <sunjw0531@gmail.com>
2023-02-15 13:35:06 +0900

    State Management : Enums

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,6 +1,12 @@
 import { atom, selector } from 'recoil';
 
-type categories = 'TO_DO' | 'DOING' | 'DONE';
+export enum Categories {
+  // 'name' 만 선언하면 그 값은 숫자값 형태로 됨.
+  // 따라서 Categories.'TO_DO' 형태는 0으로 인식됨.
+  'TO_DO' = 'TO_DO',
+  'DOING' = 'DOING',
+  'DONE' = 'DONE',
+}
 
 export const toDoState = atom<IToDo[]>({
   key: 'toDo',
@@ -10,7 +16,7 @@ export const toDoState = atom<IToDo[]>({
 export interface IToDo {
   text: string;
   id: number;
-  category: categories;
+  category: Categories;
 }
 
 export const toDoSelector = selector({
@@ -23,7 +29,7 @@ export const toDoSelector = selector({
   },
 });
 
-export const categoryState = atom<categories>({
+export const categoryState = atom<Categories>({
   key: 'category',
-  default: 'TO_DO',
+  default: Categories.TO_DO,
 });
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,7 +6,7 @@ import {
   useRecoilValue,
   useSetRecoilState,
 } from 'recoil';
-import { categoryState, toDoSelector, toDoState } from '../atoms';
+import { Categories, categoryState, toDoSelector, toDoState } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
@@ -21,9 +21,9 @@ function TodoList() {
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
       {toDos?.map((toDo) => (
